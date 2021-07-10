abbf0b8e9bd881082f1d0efece615e0a0f4d6591
emmilyyy111 <86429560+emmilyyy111@users.noreply.github.com>
2021-07-10 15:50:52 -0400

    Add files via upload

---
+++ queuePractice.js
@@ -0,0 +1,12 @@
+// implement enqueue()
+
+const ages = [7, 5, 1, 3, 4]
+// Write a function that inserts a number to the tail with two arguments
+function getEnqueue(array, num){
+    array[array.length] = num
+}
+const num1 = 9
+getEnqueue(ages, num1)
+console.log(ages)
+
+
