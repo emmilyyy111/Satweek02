ed66cb14e42c5fc0d842548e1bf7dbcc0cf1297a
emmilyyy111 <86429560+emmilyyy111@users.noreply.github.com>
2021-07-10 15:01:09 -0400

    Add files via upload

---
+++ satweek2.sql
@@ -0,0 +1,24 @@
+Use satweek2;
+
+SHOW TABLES;
+
+-- CREATE TABLE drinks(
+--     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+--     name VARCHAR(30) NOT NULL,
+--     price DECIMAL(8,2) NOT NULL,
+--     type VARCHAR(30) NOT NULL,
+--     PRIMARY KEY (id)
+-- );
+
+-- INSERT INTO drinks VALUES
+-- (101, 'Sprite', 8.99, 'Soda');
+-- (NULL, 'Dr.Pepper', 1.99, 'Soda');
+
+-- ALTER TABLE drinks ADD COLUMN diet BOOL NOT NULL;
+
+
+-- DESC drinks
+
+
+SELECT * FROM drinks;
+
